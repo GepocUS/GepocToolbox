67b0f2364f98f7728609aa2d61733169502ef449
Pablo Krupa <pkrupa@us.es>
2020-05-05 13:37:49 +0200

    Added ssMPC and EqualityMPC classes

---
+++ classes/EqualityMPC.m
@@ -0,0 +1,167 @@
+%% EqualityMPC
+% Extends the ssMPC class
+% This is a simple MPC with temirnal equality constraint and a single prediction horizon
+%
+
+% Author: Pablo Krupa (pkrupa@us.es)
+% Creation: 2020/05/03
+% Last update: 2020/05/03
+% 
+% Changelog: 
+%   v0.1 (2020/05/03): Initial commit version
+%
+
+classdef EqualityMPC < ssMPC
+    
+    methods
+    %% CONSTRUCTOR
+    function self = EqualityMPC(model, Q, R, N, varargin) % (model, Q, R, N, x0, xr, ur)
+        % Detect model class
+        if isa(model, 'ssModel')
+            is_ssModel = true;
+        else
+            is_ssModel = false;
+        end
+        % Extract from model
+        if is_ssModel
+            nx = model.n_x;
+            nu = model.n_u;
+            ny = model.n_yr;
+        else
+            nx = size(model.A, 1);
+            nu = size(model.B, 2);
+            ny = size(model.C, 1);
+        end
+        % Defaults
+        def_x0 = zeros(nx,1);
+        def_xr = zeros(nx,1);
+        def_ur = zeros(nu,1);
+        def_solver = 'quadprog';
+        % Parser
+        par = inputParser;
+        par.CaseSensitive = false;
+        par.FunctionName = 'EqualityMPC_Constructor';
+        % Required
+        addRequired(par, 'model');
+        addRequired(par, 'Q', @(x) isnumeric(x) && (size(x,1)==size(x,2)));
+        addRequired(par, 'R', @(x) isnumeric(x) && (size(x,1)==size(x,2)));
+        addRequired(par, 'N', @(x) isnumeric(x) && isscalar(x) && (x>0));
+        % Optional
+        addOptional(par, 'x0', def_x0, @(x) (isnumeric(x) && (min(size(x))==1)) || isempty(x));
+        addOptional(par, 'xr', def_xr, @(x) (isnumeric(x) && (min(size(x))==1)) || isempty(x));
+        addOptional(par, 'ur', def_ur, @(x) (isnumeric(x) && (min(size(x))==1)) || isempty(x));
+        % Name-value parameters
+        addParameter(par, 'solver', def_solver);
+        % Parse
+        parse(par, model, Q, R, N, varargin{:})
+        % Rename
+        x0 = par.Results.x0;
+        xr = par.Results.xr;
+        ur = par.Results.ur;
+        % Defauls
+        if isempty(x0); x0 = def_x0; end
+        if isempty(xr); xr = def_xr; end
+        if isempty(ur); ur = def_ur; end
+        % Make vectors column vectors
+        if size(x0,2)>1; x0=x0'; end % Make x0 a column vector
+        if size(xr,2)>1; xr=xr'; end % Make xr a column vector
+        if size(ur,2)>1; ur=ur'; end % Make ur a column vector
+        
+        % Compute ingredients of QP problem
+            % Hessian and q vector
+        [H, q] = EqualityMPC.compute_H(par.Results.Q, par.Results.R, par.Results.N);
+            % Equality constraints
+        [A, b] = EqualityMPC.compute_eq(par.Results.model, par.Results.N);
+            % Inequality constraints
+        [C, d, LB, UB] = EqualityMPC.compute_ineq(par.Results.model, par.Results.N);
+        
+        % Construct MPC
+        self@ssMPC(model, nx, nu, ny, H, q, A, b, C, d, LB, UB, par.Results.solver,...
+            par.Results.Q, par.Results.R, [], N, N, x0, xr, ur);
+        
+    end
+    
+    end
+    
+    %% PROTECTED METHODS
+    methods (Access = protected)
+       
+        function self = ref_update(self)
+            self.q = -kron(ones(self.N, 1), [self.Q*self.xr; self.R*self.ur]);
+            self.b(self.N*self.n+1:end) = self.xr;
+        end
+        
+        function self = x0_update(self)
+            self.b(1:self.n) = self.x0;
+        end
+        
+    end
+    
+    %% STATIC METHODS
+    methods (Static)
+        
+        function [H, q] = compute_H(Q, R, N)
+            if isobject(Q)
+                R = Q.R;
+                N = Q.N;
+                Q = Q.Q;
+            end
+            % Calculate the Hessian H and q vectors for the nominal MPC formulation
+            nx = size(Q, 1);
+            nu = size(R, 1);
+            H = kron(eye(N), [Q zeros(nx,nu); zeros(nu,nx) R]);
+            q = zeros(N*(nx+nu),1);
+        end
+        
+        function [Az, b] = compute_eq(model, N)
+            if isobject(model)
+                    N = model.N;
+                model = model.model;
+            end
+            A = model.A;
+            if isa(model, 'ssModel')
+                B = model.Bu;
+            else
+                B = model.B;
+            end
+            % Calculate the A and b equality matrix and vector, respectively, for the nominal MPC formulation
+            nx = size(A, 1);
+            nu = size(B, 2);
+            Az = kron(eye(N), [A B]); % Diagonal de la matriz
+            j = 0;
+            for i=1:nx:nx*N-nx % Inserto las matrices -I en Az
+                j = j+1;
+                Az(i:i+nx-1,((j-1)*(nx+nu)+(nu+nx+1)):((j-1)*(nx+nu)+(nx+nu+1)+nx-1)) = -eye(nx);
+            end
+            Az = [eye(nx) zeros(nx, nx*N-nx+nu*N); Az]; % Initial condition
+            b = zeros((N+1)*nx, 1);
+        end
+       
+        function [C, d, LB, UB] = compute_ineq(model, N)
+            if isobject(model)
+                N = model.N;
+                model = model.model;
+            end
+            A = model.A;
+            if isa(model, 'ssModel')
+                B = model.Bu;
+            else
+                B = model.B;
+            end
+            % Calculate inequality matrix and vector C and d, as well as lower and upper bounds LB and UB for the nominal MPC formulation
+            nx = size(A, 1);
+            nu = size(B, 2);
+            C = kron(eye(N), [eye(nx) zeros(nx, nu); zeros(nu, nx) eye(nu); -eye(nx) zeros(nx, nu); zeros(nu, nx) -eye(nu)]);
+            d = kron(ones(N,1), [model.UBx; model.UBu; -model.LBx; -model.LBu]);
+            LB = kron(ones(N,1), [model.LBx; model.LBu]); 
+            UB = kron(ones(N,1), [model.UBx; model.UBu]);
+        end 
+
+    end
+    
+end
+
+%% TODOS:
+% TODO: Add documentation
+% TODO: Add an example
+% TODO: Test and debug
---
+++ classes/ssMPC.m
@@ -0,0 +1,315 @@
+%% ssMPC - Abstract class for state space MPC
+% This is the abstract class for all state space based MPC controller classes
+% The non-abstract classes must define their own properties for
+%
+
+% Author: Pablo Krupa (pkrupa@us.es)
+% Creation: 2020/05/03
+% Last update: 2020/05/03
+% 
+% Changelog: 
+%   v0.1 (2020/05/03): Initial commit version
+%
+
+classdef (Abstract) ssMPC < QP
+    %% PROPERTIES
+    properties
+        model % Matrices and vectors of the system model. It must be a state space model
+        Q % Cost for the system states
+        R % Cost for the system inputs
+        P % Terminal cost
+        Nc % Control horizon
+        Np % Prediction horizon
+        x0 % Current value of system state (in incremental units)
+        x0Eng % Current value of the system state (in engineering units)
+        xr % Current value of state reference (in incremental units)
+        xrEng % Current value of state reference (in engineering units)
+        ur % Current value of input reference (in incremental units)
+        urEng % Current value of input reference (in engineering units)
+    end
+    properties (SetAccess = private)
+        n % Number of system states
+        m % Number of system inputs
+        p % Number of tracked system outputs
+        N % Short access for the control horizon (used in MPC controllers that only have one horizon)
+    end
+    properties (SetAccess = protected, Hidden = true)
+        startup_MPC % Determines if the object if being created. It avoind updates while constructor runs (is set to 0 after the constructor finishes)
+        lockSetter % Boolean used to avoid reciprocal setter loops
+    end
+    
+    methods       
+    %% CONSTRUCTOR
+    function self = ssMPC(model, nx, nu, ny, H, q, A, b, C, d, LB, UB, solver, Q, R, P, Nc, Np, x0, xr, ur)
+        self@QP(H, q, A, b, C, d, LB, UB, 'solver', solver);
+        self.startup_MPC = true;
+        self.lockSetter = false;
+        self.model = model;
+        self.Q = Q;
+        self.R = R;
+        self.P = P;
+        self.N = Nc;
+        self.Nc = Nc;
+        self.Np = Np;
+        self.n = nx;
+        self.m = nu;
+        self.p = ny;
+        self.x0 = x0;
+        self.xr = xr;
+        self.ur = ur;
+        self = self.ref_update;
+        self = self.x0_update;
+        self.startup_MPC = false;
+    end
+    
+    %% METHODS
+    function [u, z, f, exit_flag, Hist] = solve(self, varargin)
+        % Calls the QP solver
+        [z, f, exit_flag, Hist] =solve@QP(self, varargin{:});
+        u = self.extract_control(z);
+    end
+    
+    function self = setRef(self, xr, ur)
+        % Sets the state reference xr and input reference ur to the given values in incremental units. Updates the QP problem
+        self.xr = xr;
+        self.ur = ur;
+    end
+    
+    function self = setRefEng(self, xrEng, urEng)
+        % Sets the state reference xr and input reference ur to the given values in engineeging units. Updates the QP problem
+        self.xrEng = xrEng;
+        self.urEng = urEng;
+    end 
+    
+    %% SETTERS and GETTERS
+    
+    % Q
+    function self = set.Q(self, value)
+        self.Q = value;
+        if ~self.startup_MPC
+            H = compute_H(self);
+            self.H = H;
+            self = self.ref_update;
+        end
+    end
+    
+    % R
+    function self = set.R(self, value)
+        self.R = value;
+        if ~self.startup_MPC
+            H = compute_H(self);
+            self.H = H;
+            self = self.ref_update;
+        end
+    end
+    
+    % P
+    function self = set.P(self, value)
+        self.P = value;
+        if ~self.startup_MPC
+            H = compute_H(self);
+            self.H = H;
+            self = self.ref_update;
+        end
+    end
+    
+    % N
+    function self = set.N(self, value)
+        if ~self.lockSetter
+            self.lockSetter = true;
+            if ~self.startup_MPC
+                self.Nc = value;
+                self.N = value;
+                [H, q, A, b, C, d, LB, UB] = compute_QP(self);
+                self.H = H;
+                self.q = q;
+                self.A = A;
+                self.b = b;
+                self.C = C;
+                self.d = d;
+                self.LB = LB;
+                self.UB = UB;
+            else
+                self.N = value;
+            end
+        end
+        self.lockSetter = false;
+    end
+    
+    % Nc
+    function self = set.Nc(self, value)
+        if ~self.lockSetter
+            self.lockSetter = true;
+            if ~self.startup_MPC
+                self.Nc = value;
+                self.N = value;
+                [H, q, A, b, C, d, LB, UB] = compute_QP(self);
+                self.H = H;
+                self.q = q;
+                self.A = A;
+                self.b = b;
+                self.C = C;
+                self.d = d;
+                self.LB = LB;
+                self.UB = UB;
+            else
+                self.Nc = value;
+            end
+        end
+        self.lockSetter = false;
+    end
+    
+    % Np
+    function self = set.Np(self, value)
+        if ~self.startup_MPC
+            self.Np = value;
+            [H, q, A, b, C, d, LB, UB] = compute_QP(self);
+            self.H = H;
+            self.q = q;
+            self.A = A;
+            self.b = b;
+            self.C = C;
+            self.d = d;
+            self.LB = LB;
+            self.UB = UB;
+        else
+            self.Np = value;
+        end
+    end
+    
+    % x0
+    function self = set.x0(self, value)
+        if size(value,2)>1; value=value'; end % Make column vector
+        self.x0 = value;
+        if ~self.lockSetter
+            self.lockSetter = true;
+            if isa(self.model, 'ssModel')
+                self.x0Eng = x2Eng(self.model, value);
+            else
+                self.x0Eng = value;
+            end
+            if ~self.startup_MPC
+                self = self.x0_update; 
+            end
+            self.lockSetter = false;
+        end
+    end
+    function self = set.x0Eng(self, value)
+        if size(value,2)>1; value=value'; end % Make column vector
+        self.x0Eng = value;
+        if ~self.lockSetter
+            self.lockSetter = true;
+            if isa(self.model, 'ssModel')
+                self.x0 = x2Inc(self.model, value);
+            else
+                self.x0 = value;
+            end
+            if ~self.startup_MPC
+                self = self.x0_update;
+            end
+            self.lockSetter = false;
+        end
+    end
+    
+    % xr
+    function self = set.xr(self, value)
+        if size(value,2)>1; value=value'; end % Make column vector
+        self.xr = value;
+        if ~self.lockSetter
+            self.lockSetter = true;
+            if isa(self.model, 'ssModel')
+                self.xrEng = x2Eng(self.model, value);
+            else
+                self.xrEng = value;
+            end
+            if ~self.startup_MPC
+                self = self.ref_update;
+            end
+            self.lockSetter = false;
+        end
+    end
+    function self = set.xrEng(self, value)
+        if size(value,2)>1; value=value'; end % Make column vector
+        self.xrEng = value;
+        if ~self.lockSetter
+            self.lockSetter = true;
+            if isa(self.model, 'ssModel')
+                self.xr = x2Inc(self.model, value);
+            else
+                self.xr = value;
+            end
+            if ~self.startup_MPC
+                self = self.ref_update;
+            end
+            self.lockSetter = false;
+        end
+    end
+    
+    % ur
+    function self = set.ur(self, value) % Setter. Updates the QP problem
+        if size(value,2)>1; value=value'; end % Make column vector
+        self.ur = value;
+        if ~self.lockSetter
+            self.lockSetter = true;
+            if isa(self.model, 'ssModel')
+                self.urEng = u2Eng(self.model, value);
+            else
+                self.urEng = value;
+            end
+            if ~self.startup_MPC
+                self = self.ref_update;
+            end
+            self.lockSetter = false;
+        end
+    end
+    function self = set.urEng(self, value)
+        if size(value,2)>1; value=value'; end % Make column vector
+        self.urEng = value;
+        if ~self.lockSetter
+            self.lockSetter = true;
+            if isa(self.model, 'ssModel')
+                self.ur = u2Inc(self.model, value);
+            else
+                self.ur = value;
+            end
+            if ~self.startup_MPC
+                self = self.ref_update;
+            end
+            self.lockSetter = false;
+        end
+    end
+    
+    end % End of 'methods'
+    
+    %% PROTECTED METHODS
+    methods (Access = protected)
+        
+        function [H, q, A, b, C, d, LB, UB] = compute_QP(self)
+            % Computes the ingredients of the QP problem
+            [H, q] =  compute_H(self);
+            [A, b] = compute_eq(self);
+            [C, d, LB, UB] = compute_ineq(self);
+        end
+        
+    end
+
+    %% ABSTRACT METHODS
+    methods (Abstract, Access = protected)
+        
+        ref_update(self) % Updates the QP ingredients that depend on the reference
+        x0_update(self) % Updates the QP ingredients that depend on the current state
+        
+    end
+    
+    methods (Abstract, Static)
+       
+        compute_H(varargin) % Compute the H and q ingredients of the QP
+        compute_eq(varargin) % Compute the A and b ingredients of the QP (equality constraints)
+        compute_ineq(varargin) % Compute the A and b ingredients of the QP (inequality constraints)
+             
+    end
+
+end
+
+%% TODOS:
+% TODO: Add documentation
