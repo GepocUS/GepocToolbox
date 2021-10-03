b5293ab4f30c164b521c70b3a8d08e0f03b260a5
Pablo Krupa <pkrupa@us.es>
2021-10-03 20:33:34 +0200

    Bump version

--- gepoc.m
+++ gepoc.m
@@ -39,7 +39,7 @@ switch varargin{1}
     % 
     case 'version'
         
-        varargout{1} = 'v0.0.0';
+        varargout{1} = 'v0.0.1';
         
         % If git is installed it will return the hash of the current commit
         try
