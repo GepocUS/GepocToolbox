b34850aa836b2c20dd7d161cc64ae736fec77f9c
Pablo Krupa <pkrupa@us.es>
2022-10-13 14:32:16 +0200

    Added add_default_options_to_struct.m

---
+++ functions/add_default_options_to_struct.m
@@ -0,0 +1,37 @@
+%% add_default_options_to_struct
+%
+% original = add_default_options_to_struct(original, default)
+%
+% Adds the missing fields from the structure provided in the
+% argument 'default' to the structure provided in the 
+% argument 'original'.
+% 
+% It returns the 'original' structure with the new fields added.
+% 
+% This function is used to fill in the defaults of a structure
+% whose fields have not all been provided by the user.
+% It does not require knowledge of the fields of the structure.
+%
+% This function is part of GepocToolbox: https://github.com/GepocUS/GepocToolbox
+%
+
+function original = add_default_options_to_struct(original, default)
+
+    fn = fieldnames(default); % Get the field names
+    
+    for i = 1:numel(fn)
+        
+        %  Add missing fields
+        if ~isfield(original, fn{i})
+            original.(fn{i}) = default.(fn{i});
+        end
+        
+        % Fill in empty fields
+        if isempty(original.(fn{i}))
+            original.(fn{i}) = default.(fn{i});
+        end
+        
+    end
+    
+end
+
