2fcc9fbfd5ff7f5af037498c754943ae71dcc525
Pablo Krupa <pkrupa@us.es>
2022-10-13 14:34:55 +0200

    Added csrt_times_v.m

--- functions/csrt_times_v.m
+++ functions/csrt_times_v.m
@@ -1,8 +1,8 @@
-%% csr_times_v - Multiplies matrix in CSR format with a vector
+%% csrt_times_v - Multiplies the transposed of a matrix in CSR format with a vector
 % 
-%   y = csr_times_v(val, col, row_ptr, x) computes the multiplication between the
-%   vector x and the matrix in the Compressed Sparse Row (CSR) form given
-%   by the vectors val, col and row_ptr.
+%   y = csrt_times_v(val, col, row_ptr, x) computes the multiplication between the
+%   vector x and the transposed of the matrix in the Compressed Sparse Row (CSR) form
+%   given by the vectors val, col and row_ptr.
 %
 % INPUTS:
 %   - var: Vector containing the values of the non-zero elements.
@@ -13,21 +13,21 @@
 % OUTPUS:
 %   - y: Result of the matrix-vector multiplication.
 %
-% See also full2CSR, csrt_times_v
+% See also full2CSR, csr_times_v
 %
 % This function is part of the GepocToolbox: https://github.com/GepocUS/GepocToolbox
 % 
 
-function y = csr_times_v(val, col, row_ptr, x)
+function y = csrt_times_v(val, col, row_ptr, x)
 
     % Initialize
     n = length(row_ptr)-1;
-    y = zeros(n, 1);
+    y = zeros(max(col), 1);
     
     % Perform multiplication
     for i = 1:n
         for j = row_ptr(i):row_ptr(i+1)-1
-                y(i) = y(i) + val(j)*x(col(j));
+                y(col(j)) = y(col(j)) + val(j)*x(i);
         end
     end
 
