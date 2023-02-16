77e2a2d6a095e24608c10e5d32b712761a4853d0
Pablo Krupa <pkrupa@us.es>
2023-02-16 14:54:33 +0100

    Added documentation to the Oracle class

--- classes/Oracle.m
+++ classes/Oracle.m
@@ -1,12 +1,68 @@
 %% Oracle - A class for generating function oracles
 %
+% This class implements an oracle for a given function
+% f: R^n -> R^m.
+% The class provides methods to evaluate the function
+% and its derivative.
+% The objective of this class is to provide a common
+% interface for working with functions in the GepocToolbox.
+% In particular for its use in optimization algorithms.
+%
+% orac = Oracle(param, n, 'f_eval', f, 'g_eval', g)
+%
+% Constructor arguments:
+%   - param (required): Structure containing the parameters
+%                       that define the function.
+%   - n (required): Dimension of the input space of the function.
+%                   It can be a scalar (function f: R^n -> R^m) or
+%                   An array n = [n_1, n_2, ... n_p], in which case
+%                   f: R^n_1 x R^n_2 x ... x R^n_p -> R^m.
+%   - m (optional): Dimension of the output space of the function.
+%                   If not provided, it will be computed internally.
+%   - 'f_eval': Function handler to a function that evaluates the
+%               oracle function value. The function handler must
+%               have the following format:
+%               f = @(x, p) f_eval(x, p)
+%               where x is the point where the function will be 
+%               evaluated and p is the structure of parameters
+%               that define the function (constructor's param input).
+%   - 'g_eval': Function handler to a function that evaluates the
+%               oracle function gradient. The function handler must
+%               have the following format:
+%               g = @(x, p) g_eval(x, p)
+%               where x is the point where the gradient will be 
+%               evaluated and p is the structure of parameters
+%               that define the function (constructor's param input).
+%   - 'L': Lipschitz constant of the function.
+%   - 'mu': Strong convexity parameter of the function.
+%
+% Methods:
+%   - f(x_1,.. x_p): Evaluates the function value at (x_1,.. x_p)
+%   - g(x_1,.. x_p): Evaluates the gradient at (x_1,.. x_p)
+%   - set_f(f): Resets the f_eval function handler to the new handler f.
+%   - set_g(g): Resets the g_eval gradient handler to the new handler g.
+%   - get_f_hand(): Returns the function evaluation function handler.
+%   - get_g_hand(): Returns the gradient evaluation function handler.
+%
+% Simple example: Oracle for function f(x) = 0.5*x'*Q*x + c'*x
+%   n = 4; % Declare dimension
+%   param = struct('Q', eye(n), 'c', ones(n,1)); % Structure with parameters
+%   f = @(x, p) 0.5*x'*p.Q*x + p.c'*x; % Handler for function value evaluation
+%   g = @(x, p) p.Q*x + c.p; % Handler for gradient evalation
+%   L = max(eig(param.Q)); % Compute Lipschitz constant
+%   qpOracle = Oracle(param, n, 'f_eval', f, 'g_eval', g, 'L', L);
+%   qpOracle.f(rand(n, 1)) % Print f(x) for a random x
+% 
+% For additional help on a method call: help Oracle.method_name
+% 
+% This class is part of the GepocToolbox: https://github.com/GepocUS/GepocToolbox
 % 
 
 classdef Oracle < handle
 
     %% PROPERTIES
     properties
-        param
+        param % Structure containing the parameters of the function
         n % Dimension of each input variable
         m % Output dimension of the function
         dim % Number of input variables of the function
@@ -63,12 +119,7 @@ classdef Oracle < handle
 
         % Try to set other properties
         if isempty(self.m)
-            self.update_m_();
-            % if ~isempty(self.f_eval_hand_)
-            %     point = cell(1, self.dim);
-            %     [point{:}] = self.rand_point_();
-            %     self.m = length(self.f(point{:}));
-            % end
+            self.update_m_(); % Automatically set m
         end
 
     end
